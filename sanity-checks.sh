e774617b6e945ee8d162af1c0b0ae54c754eeaa2
ryanrudolfoba <98122529+ryanrudolfoba@users.noreply.github.com>
2026-07-14 09:43:20 -0400

    updated for SteamOS 3.8

--- sanity-checks.sh
+++ sanity-checks.sh
@@ -11,14 +11,13 @@ else
 	exit
 fi
 
-# sanity check - make sure this is running on at least SteamOS 3.7.x
-# if awk "BEGIN {exit ! ($STEAMOS_VERSION >= $BASE_VERSION)}" - comment this out as there are issues with 3.8 onwards
+# sanity check - make sure this is running on at least SteamOS 3.8.x
 if awk "BEGIN {exit ! ($STEAMOS_VERSION >= $BASE_VERSION && $STEAMOS_VERSION <= 3.8)}"
 then
 	echo SteamOS $STEAMOS_VERSION $STEAMOS_BRANCH detected. Proceed to the next step.
 else
 	echo SteamOS $STEAMOS_VERSION $STEAMOS_BRANCH detected. This is unsupported version.
-	echo Update SteamOS and make sure it is at least on SteamOS 3.7.x.
+	echo Update SteamOS and make sure it is at least on SteamOS 3.8.x.
 	exit
 fi
 
